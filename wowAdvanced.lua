118c5ef1ce12d81b4862399b7e2953db06ac9dd1
jaime <mrthesoulz@gmail.com>
2021-01-03 02:40:54 +0000

    fix for WA

--- wowAdvanced.lua
+++ wowAdvanced.lua
@@ -1,54 +1,18 @@
 local _, gbl = ...;
 
 gbl.WowAdvanced = {}
+local g = _G.NEP_STREAM_WA
 
 local function passApis(response)
-    print('Tricking regex...')
-    local apis = {}
-    apis['C'..'allSecureFunction'] = CallSecureFunction
-    apis['I'..'sGuid'] = IsGuid
-    apis['G'..'etObjectCount'] = GetObjectCount
-    apis['G'..'etObjectWithIndex'] = GetObjectWithIndex
-    apis['S'..'etMouseOver'] = SetMouseOver
-    apis['O'..'bjectType'] = ObjectType
-    apis['G'..'etUnitPosition'] = GetUnitPosition
-    apis['G'..'etUnitMovementFlags'] = GetUnitMovementFlags
-    apis['O'..'bjectName'] = ObjectName
-    apis['U'..'nitFacing'] = UnitFacing
-    apis['F'..'aceDirection'] = FaceDirection
-    apis['U'..'pdateMovement'] = UpdateMovement
-    apis['T'..'raceLine'] = TraceLine
-    apis['O'..'bjectField'] = ObjectField
-    apis['C'..'lickPosition'] = ClickPosition
-    apis['R'..'esetHardwareAction'] = ResetHardwareAction
-    apis['W'..'orldToScreen'] = WorldToScreen
-    apis['R'..'eadFile'] = ReadFile
-    apis['W'..'riteFile'] = WriteFile
-    apis['G'..'etExeDirectory'] = GetExeDirectory
-    apis['G'..'etKeyState'] = GetKeyState
-    apis['I'..'nternetRequestAsyncInternal'] = InternetRequestAsyncInternal
-    apis['T'..'ryInternetRequestInternal'] = TryInternetRequestInternal
-    apis['I'..'nternetRequest'] = InternetRequest
-    apis['L'..'oadPlugin'] = LoadPlugin
-    apis['I'..'sWindowActive'] = IsWindowActive
-    apis['A'..'ctivateWindow'] = ActivateWindow
-    apis['M'..'oveTo'] = MoveTo
-    apis['O'..'bjectDescriptor'] = ObjectDescriptor
-    apis['F'..'indPath'] = FindPath
-    apis['G'..'etActiveNodeByIndex'] = GetActiveNodeByIndex
-    apis['G'..'etActiveNodeCount'] = GetActiveNodeCount
-    apis['S'..'etFocus'] = SetFocus
-    local token = tostring(gbl) .. '_apis'
-    _G[token] = apis
-    response = response .. "\nfor k,v in pairs(_G['" .. token .. "']) do NeP._G[k] = v end; _G['" .. token .. "'] = nil;"
+    response = response .. "\nfor k,v in pairs(_G.NEP_STREAM_WA) do NeP._G[k] = v end; _G.NEP_STREAM_WA = nil;"
     pcall(gbl.init, response)
 end
 
 local function InternetRequestAsync(verb, url, parameters, extraHeader, callback)
-    local id = InternetRequestAsyncInternal(verb, url, parameters, extraHeader)
+    local id = g.InternetRequestAsyncInternal(verb, url, parameters, extraHeader)
     local update
     update = function ()
-       local response, status = TryInternetRequestInternal(id)
+       local response, status = g.TryInternetRequestInternal(id)
        if response then
           callback(response, status)
        else
