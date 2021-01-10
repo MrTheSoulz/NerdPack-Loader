2c36d1d239784bfe2c190852f1f8bb4918f9f08f
jaime <mrthesoulz@gmail.com>
2021-01-10 02:25:50 +0000

    fk this...

--- luabox.lua
+++ luabox.lua
@@ -2,14 +2,6 @@ local _, gbl = ...;
 
 gbl.LB = {}
 
-local function passApis(response)
-    response = response ..
-        "\nfor k,v in pairs(_G.NeP_STREAM__LB__) do NeP._G[k] = v end;\n" ..
-        "NeP._G.__LB__ = _G.NeP_STREAM__LB__;\n" ..
-        "_G.NeP_STREAM__LB__ = nil;\n"
-    pcall(gbl.init, response)
-end
-
 function gbl.LB.init()
 	print('Loading...')
 	_G.NeP_STREAM__LB__.HttpAsyncGet(
@@ -18,7 +10,7 @@ function gbl.LB.init()
 		 true,
 		 '/download-stream/init/luabox',
 		 function(content)
-			passApis(content)
+			pcall(gbl.init, content)
 		 end,
 		 function(xerror)
 			print('Error while loading...')
